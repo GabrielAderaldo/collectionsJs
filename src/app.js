63d3aeb361206a647dd4c824f30e644753fc87c0
Gabriel Aderaldo <gabriel_aderaldo@edu.unifor.br>
2024-05-19 13:45:10 -0300

    [01] - Adicionando um exemplo.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 import { Graph } from "./graph/graph.js";
 
-
 export default function app(){
-    console.log("Graph Data Structure");
+
+    const graph = new Graph();
+    let vertice = ["A", "B", "C", "D", "E", "F"];
+
+    for(let i = 0; i < vertice.length; i++){
+        graph.addVertex(vertice[i]);
+    }
+
+    graph.addEdge("A", "B");
+    graph.addEdge("A", "C");
+    graph.addEdge("B", "D");
+    graph.addEdge("C", "E");
+    graph.addEdge("D", "E");
+    graph.addEdge("D", "F");
+    graph.addEdge("E", "F");
+
 }
